71a70745ee6a9c14b272c19220af57a37fd908a3
filip szeszko <filip@biznesport.pl>
2020-01-23 00:07:02 +0100

    adding theme

---
+++ wp-content/themes/canvas/header.php
@@ -0,0 +1,37 @@
+<header id="header" class="full-header transparent-header dark static-sticky" data-sticky-class="not-dark" data-sticky-offset="full" data-sticky-offset-negative="100">
+
+			<div id="header-wrap">
+
+				<div class="container clearfix">
+
+					<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+					<!-- Logo
+					============================================= -->
+					<div id="logo">
+						<a href="index.php" class="standard-logo" data-dark-logo="wp-content/themes/canvas/images/logo-light.png"><img src="wp-content/themes/canvas/images/logo-dark.png" alt="Canvas Logo"></a>
+						<a href="index.php" class="retina-logo" data-dark-logo="wp-content/themes/canvas/images/canvasone-dark@2x.png"><img src="wp-content/themes/canvas/images/canvasone@2x.png" alt="Canvas Logo"></a>
+					</div><!-- #logo end -->
+
+					<!-- Primary Navigation
+					============================================= -->
+					<nav id="primary-menu">
+
+						<ul class="one-page-menu" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
+							<li><a href="#" data-href="#wrapper"><div>Start</div></a></li>
+							<li><a href="#" data-href="#section-about"><div>O nas</div></a></li>
+							<li><a href="#" data-href="#section-works"><div>Technologie</div></a></li>
+							<li><a href="#" data-href="#section-services"><div>Usługi</div></a></li>
+							<li><a href="#" data-href="#section-blog"><div>Referencje</div></a></li>
+							<li><a href="#" data-href="#section-contact"><div>Portfolio</div></a></li>
+							<li><a href="#" data-href="#section-contact"><div>Kontakt</div></a></li>
+						</ul>
+
+						<div id="side-panel-trigger" class="side-panel-trigger"><a href="#"><i class="icon-reorder"></i></a></div>
+
+					</nav><!-- #primary-menu end -->
+
+				</div>
+			</div>
+
+		</header>
